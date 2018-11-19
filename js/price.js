54712182939f8bb41343e057dcac5b4c80e9c8ea
Rodrigo Costa <svork@users.noreply.github.com>
2018-11-19 13:49:37 -0200

    Valor saldo devedor atual e anterior

--- js/price.js
+++ js/price.js
@@ -9,22 +9,26 @@ var n = localStorage.getItem("n");
 // Variáveis
 var R;
 var Pt;
+var Pt1;
+
+// Fórmulas
+function valor_R(){
+  R = P * (((( 1 + i ) ** n ) * i ) / ((( 1 + i ) ** n ) - 1 ));
+  return R;
+}
 
 function p01(){
-  // Fórmula
-  R = P * (((( 1 + i ) ** n ) * i ) / ((( 1 + i ) ** n ) - 1 ));
-
-  // Resultado
-  document.getElementById("resultado").innerHTML = "O valor da prestação é R$ " + R.toFixed(2);
+  document.getElementById("resultado").innerHTML = "O valor da prestação é R$ " + valor_R().toFixed(2);
 }
 
 function p02(){
   var t = document.getElementById("p02t").value;
-
-  // Fórmula
-  R = P * (((( 1 + i ) ** n ) * i ) / ((( 1 + i ) ** n ) - 1 ));
-  Pt = R * ((( 1 + i ) ** ( n - t )) - 1 ) / (((( 1 + i ) ** ( n - t ) * i )))
-
-  // Resultado
+  Pt = valor_R() * ((( 1 + i ) ** ( n - t )) - 1 ) / (((( 1 + i ) ** ( n - t ) * i )))
   document.getElementById("resultado").innerHTML = "O valor do saldo devedor ao final da prestação " + t + " é R$ " + Pt.toFixed(2);
+}
+
+function p03(){
+  var t = document.getElementById("p03t").value;
+  Pt1 = valor_R() * ((( 1 + i ) ** (( n - t ) + 1 )) - 1 ) / (((( 1 + i ) ** (( n - t ) + 1 ) * i )))
+  document.getElementById("resultado").innerHTML = "O valor do saldo devedor ao final da prestação " + ( t - 1 ) + " é R$ " + Pt1.toFixed(2);
 }
