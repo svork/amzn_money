731e477d05dcca60b73761737c7191a73def937f
Rodrigo Costa <svork@users.noreply.github.com>
2018-11-19 17:43:34 -0200

    Valor saldo devedor

--- js/sac.js
+++ js/sac.js
@@ -8,6 +8,7 @@ var n = localStorage.getItem("n");
 
 // Variáveis
 var A;
+var Pt;
 
 // Fórmulas
 function valor_A(){
@@ -15,7 +16,17 @@ function valor_A(){
   return A;
 }
 
+function valor_Pt(n, t){
+  Pt = valor_A() * ( n - t );
+  return Pt;
+}
+
 // Funções
 function s01(){
   document.getElementById("resultado").innerHTML = "O valor da amortização é R$ " + valor_A().toFixed(2);
+}
+
+function s02(){
+  var t = document.getElementById("s02t").value;
+  document.getElementById("resultado").innerHTML = "O saldo devedor na parcela " + t + " é R$ " + valor_Pt(n, t).toFixed(2);
 }
