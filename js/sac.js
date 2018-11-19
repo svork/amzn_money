3865f3f9d1bb04f358a7aedf99ec4afdd876703c
Rodrigo Costa <svork@users.noreply.github.com>
2018-11-19 17:49:56 -0200

    Valor do saldo devedor anterior

--- js/sac.js
+++ js/sac.js
@@ -9,6 +9,7 @@ var n = localStorage.getItem("n");
 // Variáveis
 var A;
 var Pt;
+var Pt1;
 
 // Fórmulas
 function valor_A(){
@@ -21,6 +22,11 @@ function valor_Pt(n, t){
   return Pt;
 }
 
+function valor_Pt1(n, t){
+  Pt1 = valor_A() * ( n - t + 1 );
+  return Pt1;
+}
+
 // Funções
 function s01(){
   document.getElementById("resultado").innerHTML = "O valor da amortização é R$ " + valor_A().toFixed(2);
@@ -29,4 +35,9 @@ function s01(){
 function s02(){
   var t = document.getElementById("s02t").value;
   document.getElementById("resultado").innerHTML = "O saldo devedor na parcela " + t + " é R$ " + valor_Pt(n, t).toFixed(2);
+}
+
+function s03(){
+  var t = document.getElementById("s03t").value;
+  document.getElementById("resultado").innerHTML = "O saldo devedor na parcela " + ( t - 1 ) + " é R$ " + valor_Pt1(n, t).toFixed(2);
 }
